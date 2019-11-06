16874ced52c54863a631af69013c38b1b90df7b1
savithande <38084956+savithande@users.noreply.github.com>
2019-11-06 11:42:20 +0530

    Add files via upload

---
+++ numbers.py
@@ -0,0 +1,45 @@
+#assignment af numbers to the variable
+
+int_var1 = 10
+int_var2 = 20
+
+#addition of 2 numbers
+
+print(int_var1 + int_var2)
+print ("\n")
+
+#assignment of floating point number
+float_num1 = 5.6
+float_num2 = 5.8
+
+#adition of floating point numbers
+print (float_num1 + float_num2)
+print("\n")
+
+#adition of intiger numer and the floating point number
+print(int_var1 + float_num1)
+print("\n")
+
+#assignment of complex numbers
+complex_num1 = 3 + 4j
+complex_num2 = 5 + 7j
+
+#adition of complex_numbers
+print (complex_num1 + complex_num2)
+print ("\n")
+
+#adition of complex number with integer value
+result = (6 +8j) + 4
+print (result)
+print ("\n")
+
+print(complex_num1 + int_var1)
+print ("\n")
+
+#adition with complex number with floating number
+ans = (4 +9j) + 5.7784
+print (ans)
+print ("\n")
+
+print (complex_num1 + float_num1)
+print ("\n")
